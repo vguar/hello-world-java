60cef4fcf4b31859ea892de3c1f99502fbfd3070
rhoa <rhoa@users.noreply.github.com>
2017-05-03 09:39:01 +0200

    Update IndexController.java

--- src/main/java/hello/IndexController.java
+++ src/main/java/hello/IndexController.java
@@ -1,12 +1,33 @@
 package hello;
 
+import java.net.InetAddress;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.ui.Model;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+
 @Controller
 public class IndexController {
+
+	private String hostname;
+
+	@Value( "${spring.application.version:v1.0.0}")
+	private String version;
+
+	public IndexController() throws Exception {
+		hostname = InetAddress.getLocalHost().getHostName();
+	}
+
     @RequestMapping("/")
-    String index(){
+    public String greeting(Model model) throws Exception{
+
+        model.addAttribute("hostname", this.hostname);
+        model.addAttribute("version", version);
+
         return "index";
     }
+
 }
+
